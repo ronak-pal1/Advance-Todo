aa24471c93b767cd4e5ef18959541709b069b510
ronak-pal1 <ronakpaul882@gmail.com>
2024-10-19 00:55:27 +0530

    Dashboard page added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,19 @@
+import Header from "../components/Header";
+import MainDash from "../components/MainDash";
+import Sidebar from "../components/Sidebar";
+
 const Dashboard = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <div className="flex flex-1 h-screen">
+        {/* Sidebar */}
+        <Sidebar />
+        {/* Main Dashboard window */}
+        <MainDash />
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
